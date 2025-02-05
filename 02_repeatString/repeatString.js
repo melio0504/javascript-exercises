9bac6c3f85a7e78b59f0d96faaa40f07caee58a2
melio0504 <82772382+melio0504@users.noreply.github.com>
2025-02-06 01:04:05 +0800

    finished first exercise and currently in the middle of second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let result = "";
 
+    for (let i = 0; i < num; i++) {
+        result += str;
+    }
+
+    return result;
 };
+
+repeatString('goodbye', -1);
 
 // Do not edit below this line
 module.exports = repeatString;
