47c6039fa1b11dfea01ae407dd844d8b4c6397d2
melio0504 <82772382+melio0504@users.noreply.github.com>
2025-02-10 22:09:32 +0800

    finish reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    return string.split("").reverse().join("");
 };
+
+reverseString('');
 
 // Do not edit below this line
 module.exports = reverseString;
