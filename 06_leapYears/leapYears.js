7340298d457ffe94c7c639d645253d91aa926bbc
melio0504 <82772382+melio0504@users.noreply.github.com>
2025-02-10 23:17:39 +0800

    finish leapYears exercises

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const divisibleByFour = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const yearDivisibleByFourHundred = year % 400 === 0;
 
+    if (divisibleByFour && (!isCentury || yearDivisibleByFourHundred)) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+leapYears(700);
 
 // Do not edit below this line
 module.exports = leapYears;
